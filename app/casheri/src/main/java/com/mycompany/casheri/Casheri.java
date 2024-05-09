eb8a99b73cb258df50c858dd2c55372e3ceb8b14
diasakosd <103368009+diasakosd@users.noreply.github.com>
2024-05-09 19:41:10 +0300

    gg

---
+++ app/casheri/src/main/java/com/mycompany/casheri/Casheri.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.casheri;
+
+/**
+ *
+ * @author Damianos
+ */
+public class Casheri {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
