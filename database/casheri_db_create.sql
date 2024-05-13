0ef84d252e2f365f648fb73689b2b82f90c14c58
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2024-05-13 17:16:02 +0300

    create&insert commands

---
+++ database/casheri_db_create.sql
@@ -0,0 +1,72 @@
+create database casheri;
+use casheri;
+CREATE TABLE `User`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL,
+    `full_name` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `phone` BIGINT NOT NULL,
+    `latitude` DOUBLE(8, 2) NOT NULL,
+    `longitude` DOUBLE(8, 2) NOT NULL
+);
+ALTER TABLE
+    `User` ADD UNIQUE `user_username_unique`(`username`);
+ALTER TABLE
+    `User` ADD UNIQUE `user_phone_unique`(`phone`);
+CREATE TABLE `Driver`(
+    `user_id` INT UNSIGNED NOT NULL,
+    `car_model` VARCHAR(255) NOT NULL,
+    `car_id` VARCHAR(255) NOT NULL,
+    `car_color` VARCHAR(255) NOT NULL,
+    `max_passenger_capacity` INT NOT NULL,
+    PRIMARY KEY(`user_id`)
+);
+ALTER TABLE
+    `Driver` ADD UNIQUE `driver_car_id_unique`(`car_id`);
+CREATE TABLE `Trip`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `driver_id` INT UNSIGNED NOT NULL,
+    `date_time` DATETIME NOT NULL,
+    `start_latitude` DOUBLE(8, 2) NOT NULL,
+    `start_longitude` DOUBLE(8, 2) NOT NULL,
+    `end_latitude` DOUBLE(8, 2) NOT NULL,
+    `end_longitude` DOUBLE(8, 2) NOT NULL,
+    `cost` FLOAT NOT NULL
+);
+CREATE TABLE `Passenger`(
+    `user_id` INT UNSIGNED NOT NULL,
+    `total_trips` INT NOT NULL,
+    `reviews_rank` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`user_id`)
+);
+CREATE TABLE `Post`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `driver_id` INT UNSIGNED NOT NULL,
+    `trip_id` INT UNSIGNED NOT NULL,
+    `post_datetime` DATETIME NOT NULL
+);
+CREATE TABLE `Ride`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `trip_id` INT UNSIGNED NOT NULL,
+    `passenger_id` INT UNSIGNED NOT NULL,
+    `date_time` DATETIME NOT NULL,
+    `start_latitude` DOUBLE(8, 2) NOT NULL,
+    `start_longitude` DOUBLE(8, 2) NOT NULL,
+    `end_latitude` DOUBLE(8, 2) NOT NULL,
+    `end_longitude` DOUBLE(8, 2) NOT NULL,
+    `cost` DOUBLE(8, 2) NOT NULL
+);
+ALTER TABLE
+    `Post` ADD CONSTRAINT `post_driver_id_foreign` FOREIGN KEY(`driver_id`) REFERENCES `Driver`(`user_id`);
+ALTER TABLE
+    `Ride` ADD CONSTRAINT `ride_trip_id_foreign` FOREIGN KEY(`trip_id`) REFERENCES `Trip`(`id`);
+ALTER TABLE
+    `Post` ADD CONSTRAINT `post_trip_id_foreign` FOREIGN KEY(`trip_id`) REFERENCES `Trip`(`id`);
+ALTER TABLE
+    `Driver` ADD CONSTRAINT `driver_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `User`(`id`);
+ALTER TABLE
+    `Trip` ADD CONSTRAINT `trip_driver_id_foreign` FOREIGN KEY(`driver_id`) REFERENCES `Driver`(`user_id`);
+ALTER TABLE
+    `Ride` ADD CONSTRAINT `ride_passenger_id_foreign` FOREIGN KEY(`passenger_id`) REFERENCES `Passenger`(`user_id`);
+ALTER TABLE
+    `Passenger` ADD CONSTRAINT `passenger_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `User`(`id`);
