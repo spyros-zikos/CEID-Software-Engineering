e84620252baa7eed0740fdcc89f37c966491073e
diasakosd <103368009+diasakosd@users.noreply.github.com>
2024-05-16 02:45:45 +0300

    gg

---
+++ app/casheri/src/main/java/com/mycompany/casheri/Casheri.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.casheri;
+
+import my.casheri.MenuUI;
+
+/**
+ *
+ * @author Damianos
+ */
+public class Casheri {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new MenuUI().setVisible(true);
+    }
+}
