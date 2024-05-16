53189a9c236e0ca63e72dd2d8854399ff6403587
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2024-05-16 23:05:17 +0300

    change name to some classes

--- app/casheri/src/main/java/my/casheri/DriverUI.java
+++ app/casheri/src/main/java/my/casheri/DriverUI.java
@@ -6,12 +6,12 @@ import javax.swing.ImageIcon;
  *
  * @author Damianos
  */
-public class casheriUI extends javax.swing.JFrame {
+public class DriverUI extends javax.swing.JFrame {
 
     /**
      * Creates new form casheriUI
      */
-    public casheriUI() {
+    public DriverUI() {
         initComponents();
     }
 
@@ -147,20 +147,21 @@ public class casheriUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(casheriUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DriverUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(casheriUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DriverUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(casheriUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DriverUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(casheriUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DriverUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new casheriUI().setVisible(true);
+                new DriverUI().setVisible(true);
             }
         });
     }
