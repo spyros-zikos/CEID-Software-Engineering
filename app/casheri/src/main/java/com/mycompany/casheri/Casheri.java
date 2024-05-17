681e7b2832a9518da7c111170170c60bf3642354
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2024-05-17 22:26:21 +0300

    trying to implement route

--- app/casheri/src/main/java/com/mycompany/casheri/Casheri.java
+++ app/casheri/src/main/java/com/mycompany/casheri/Casheri.java
@@ -5,6 +5,7 @@
 package com.mycompany.casheri;
 
 import my.casheri.MenuUI;
+import my.casheri.casheriUI;
 
 /**
  *
@@ -14,6 +15,7 @@ public class Casheri {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        new MenuUI().setVisible(true);
+//        new MenuUI().setVisible(true);
+        new casheriUI().setVisible(true);
     }
 }
