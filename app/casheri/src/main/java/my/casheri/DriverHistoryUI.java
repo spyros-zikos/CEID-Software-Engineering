ddfc7646ed802e6904deb293816ce38dee21e46c
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2024-05-24 12:34:25 +0300

    finished DirverHistoryUI

--- app/casheri/src/main/java/my/casheri/DriverHistoryUI.java
+++ app/casheri/src/main/java/my/casheri/DriverHistoryUI.java
@@ -2,21 +2,30 @@ package my.casheri;
 
 import com.mycompany.casheri.Database;
 import com.mycompany.casheri.Trip;
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.io.FileWriter;
+import java.io.IOException;
 import static java.lang.Math.round;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class DriverHistoryUI extends javax.swing.JFrame {
@@ -36,9 +45,11 @@ public class DriverHistoryUI extends javax.swing.JFrame {
     
     private void init() {
         initComponents();
+        //Design
         this.setSize(296,455);
         this.setLocationRelativeTo(null);
         this.getContentPane().setBackground(Color.decode("#FFFFBA"));
+        //End
         getCompletedTrips();
         displayTable();
     }
@@ -106,7 +117,7 @@ public class DriverHistoryUI extends javax.swing.JFrame {
     }
     
     private void displayTable(){
-        DefaultTableModel model = (DefaultTableModel)jTable1.getModel();
+        DefaultTableModel model = (DefaultTableModel)statsTable.getModel();
         Object []  row = new Object [5];
         for(int i=0; i<completedTrips.size(); i++){
             row[0]= completedTrips.get(i).getDatetime();
@@ -118,6 +129,28 @@ public class DriverHistoryUI extends javax.swing.JFrame {
         }
     }
     
+    private String calculateTotalDuration() {
+        List<String> timeStrings = new ArrayList<>();
+        for(int i=0; i<completedTrips.size(); i++) {
+            timeStrings.add(completedTrips.get(i).getDuration());
+        }
+        
+        Duration totalDuration = Duration.ZERO;
+
+        for (String timeString : timeStrings) {
+            LocalTime time = LocalTime.parse(timeString, DateTimeFormatter.ofPattern("HH:mm:ss"));
+            Duration duration = Duration.between(LocalTime.MIN, time);
+            totalDuration = totalDuration.plus(duration);
+        }
+
+        long totalSeconds = totalDuration.getSeconds();
+        long hours = totalSeconds / 3600;
+        long minutes = (totalSeconds % 3600) / 60;
+        long seconds = totalSeconds % 60;
+        
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -127,61 +160,66 @@ public class DriverHistoryUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton4 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jPanel1 = new javax.swing.JPanel();
+        calculateButton = new javax.swing.JButton();
+        downloadButton = new javax.swing.JButton();
+        menuButton = new javax.swing.JButton();
+        filtersButton = new javax.swing.JButton();
+        barChartPane = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        statsTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(245, 245, 245));
 
-        jButton4.setBackground(new java.awt.Color(236, 218, 61));
-        jButton4.setText("Calculate");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        calculateButton.setBackground(new java.awt.Color(236, 218, 61));
+        calculateButton.setText("Calculate");
+        calculateButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                calculateButtonActionPerformed(evt);
             }
         });
 
-        jButton3.setBackground(new java.awt.Color(236, 218, 61));
-        jButton3.setText("Download");
-
-        jButton2.setBackground(new java.awt.Color(236, 218, 61));
-        jButton2.setText("Menu");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        downloadButton.setBackground(new java.awt.Color(236, 218, 61));
+        downloadButton.setText("Download");
+        downloadButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                downloadButtonActionPerformed(evt);
             }
         });
 
-        jButton1.setBackground(new java.awt.Color(236, 218, 61));
-        jButton1.setText("Filters");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        menuButton.setBackground(new java.awt.Color(236, 218, 61));
+        menuButton.setText("Menu");
+        menuButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                menuButtonActionPerformed(evt);
             }
         });
 
-        jPanel1.setBackground(new java.awt.Color(255, 250, 228));
+        filtersButton.setBackground(new java.awt.Color(236, 218, 61));
+        filtersButton.setText("Filters");
+        filtersButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                filtersButtonActionPerformed(evt);
+            }
+        });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        barChartPane.setBackground(new java.awt.Color(255, 250, 228));
+
+        javax.swing.GroupLayout barChartPaneLayout = new javax.swing.GroupLayout(barChartPane);
+        barChartPane.setLayout(barChartPaneLayout);
+        barChartPaneLayout.setHorizontalGroup(
+            barChartPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 0, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        barChartPaneLayout.setVerticalGroup(
+            barChartPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 135, Short.MAX_VALUE)
         );
 
         jScrollPane1.setPreferredSize(new java.awt.Dimension(280, 200));
 
-        jTable1.setBackground(new java.awt.Color(255, 250, 228));
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        statsTable.setBackground(new java.awt.Color(255, 250, 228));
+        statsTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -197,12 +235,12 @@ public class DriverHistoryUI extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jTable1.setPreferredSize(new java.awt.Dimension(290, 100));
-        jTable1.setSelectionBackground(new java.awt.Color(153, 153, 0));
-        jTable1.getTableHeader().setReorderingAllowed(false);
-        jScrollPane1.setViewportView(jTable1);
-        if (jTable1.getColumnModel().getColumnCount() > 0) {
-            jTable1.getColumnModel().getColumn(2).setPreferredWidth(100);
+        statsTable.setPreferredSize(new java.awt.Dimension(290, 100));
+        statsTable.setSelectionBackground(new java.awt.Color(153, 153, 0));
+        statsTable.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(statsTable);
+        if (statsTable.getColumnModel().getColumnCount() > 0) {
+            statsTable.getColumnModel().getColumn(2).setPreferredWidth(100);
         }
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -215,19 +253,19 @@ public class DriverHistoryUI extends javax.swing.JFrame {
                 .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(34, 34, 34)
-                .addComponent(jButton2)
+                .addComponent(menuButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(filtersButton)
                 .addGap(36, 36, 36))
             .addGroup(layout.createSequentialGroup()
                 .addGap(25, 25, 25)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(calculateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(downloadButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(25, 25, 25))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(barChartPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -235,60 +273,104 @@ public class DriverHistoryUI extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(filtersButton)
+                    .addComponent(menuButton))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(barChartPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton4)
-                    .addComponent(jButton3))
+                    .addComponent(calculateButton)
+                    .addComponent(downloadButton))
                 .addContainerGap(81, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void filtersButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_filtersButtonActionPerformed
         // TODO add your handling code here:
         new FiltersUI().setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_filtersButtonActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void menuButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuButtonActionPerformed
         // TODO add your handling code here:
         new casheriUI().setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_menuButtonActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void calculateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_calculateButtonActionPerformed
         // TODO add your handling code here:
         DefaultCategoryDataset barChartData = new DefaultCategoryDataset();
         float sumProfit = 0;
         int sumPassengers = 0;
-        
+                
         for(int i=0; i<completedTrips.size(); i++) {
             sumProfit += completedTrips.get(i).getProfit();
         }
         for(int i=0; i<completedTrips.size(); i++) {
             sumPassengers += completedTrips.get(i).getPassengers();
         }
+        
         barChartData.setValue(sumProfit,"","Profit");
         barChartData.setValue(sumPassengers,"","Passengers");
-        barChartData.setValue(completedTrips.size(),"","Total Trips");
+        barChartData.setValue(completedTrips.size(),"","Trips");
 
-        JFreeChart barChart = ChartFactory.createBarChart("title", "", "", barChartData,PlotOrientation.VERTICAL, false,true,false);
+        String range = null;
+        if (filter)
+            range = selectedFilters.getStartDateTime()+" - "+ selectedFilters.getEndDateTime();
+        else
+            range = "All Time";
+        
+        JFreeChart barChart = ChartFactory.createBarChart(range, "Duration: " + calculateTotalDuration(), "", barChartData,PlotOrientation.VERTICAL, false,true,false);
+        
+        barChart.getTitle().setFont(new Font("SansSerif", Font.PLAIN, 12));
+        BarRenderer renderer = new BarRenderer();
+        renderer.setSeriesPaint(0, new Color(159, 159, 81));
+        barChart.getCategoryPlot().setRenderer(renderer);
+        
         CategoryPlot barchrt = barChart.getCategoryPlot();
         barchrt.setRangeGridlinePaint(Color.BLACK);
   
         ChartPanel chartPanel  = new ChartPanel(barChart);
         chartPanel.setSize(260,135);
-        jPanel1.removeAll();
-        jPanel1.add(chartPanel);//, BorderLayout.CENTER
-        jPanel1.updateUI();
-    }//GEN-LAST:event_jButton4ActionPerformed
+        barChartPane.removeAll();
+        barChartPane.add(chartPanel);//, BorderLayout.CENTER
+        barChartPane.updateUI();
+    }//GEN-LAST:event_calculateButtonActionPerformed
+
+    private void downloadButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_downloadButtonActionPerformed
+        // TODO add your handling code here:      
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Save as");
+        fileChooser.setPreferredSize(new Dimension(290,420));
+        
+        int userSelection = fileChooser.showSaveDialog(this);
+        
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            try (FileWriter fileWriter = new FileWriter(fileChooser.getSelectedFile())) {
+                TableModel model = statsTable.getModel();
+
+                for (int i = 0; i < model.getColumnCount(); i++) {
+                    fileWriter.write(model.getColumnName(i) + "\t");
+                }
+                fileWriter.write("\n");
+
+                for (int i = 0; i < model.getRowCount(); i++) {
+                    for (int j = 0; j < model.getColumnCount(); j++) {
+                        fileWriter.write(model.getValueAt(i, j).toString() + "\t");
+                    }
+                    fileWriter.write("\n");
+                }
+                
+                JOptionPane.showMessageDialog(this, "File saved successfully!");
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(this, "Error saving file: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_downloadButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -326,12 +408,12 @@ public class DriverHistoryUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel barChartPane;
+    private javax.swing.JButton calculateButton;
+    private javax.swing.JButton downloadButton;
+    private javax.swing.JButton filtersButton;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JButton menuButton;
+    private javax.swing.JTable statsTable;
     // End of variables declaration//GEN-END:variables
 }
